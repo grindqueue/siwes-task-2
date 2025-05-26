ecbc4768f125d9ce8a2bce458ef9c8e8d99cf2c6
Ope <omolewa.opeyemi@lmu.edu.ng>
2025-05-26 11:36:42 +0100

    Fix: update the userrouter file

--- Router/userRouter.js
+++ Router/userRouter.js
@@ -9,4 +9,4 @@ userRouter.get("/users", (req, res) => getAllUsers(req, res));
 userRouter.get("/:id", authorize, (req, res) => getUser(req, res));
 
 
-module.exports = {userRouter};
+module.exports = userRouter;
