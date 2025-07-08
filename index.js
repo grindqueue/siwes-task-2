1d0e526797bde1968647ac1e20fd323cb2abb6c3
Ope <omolewa.opeyemi@lmu.edu.ng>
2025-07-08 11:35:23 +0100

    use cors to allow all routes

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+    origin : "*";
+}));
 app.use(helmet());
 
 
